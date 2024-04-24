a9563fecc53fcaf5d5162ad85011ff96ff1e0a16
Ivan Mincik <ivan.mincik@gmail.com>
2024-04-24 10:57:32 +0200

    geonixcli: move customized packages function example to geonix.nix file

--- pkgs/geonixcli/templates/init/geonix.nix
+++ pkgs/geonixcli/templates/init/geonix.nix
@@ -5,6 +5,16 @@
 { inputs, config, lib, pkgs, ... }:
 
 let
+  # CUSTOMIZED PACKAGES
+  # Run `nix run .#geonixcli -- override` and replace geopkgs variable with
+  # function below to use customized packages defined by `overrides.nix` file.
+
+  # geopkgs = inputs.geonix.lib.customizePackages {
+  #   nixpkgs = pkgs;
+  #   geopkgs = inputs.geonix.packages.${pkgs.system};
+  #   overridesFile = ./overrides.nix;
+  # };
+
   geopkgs = inputs.geonix.packages.${pkgs.system};
 
 in {
